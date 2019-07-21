9c007f99cb366d78d7600b71f4cc5411bb2b71c3
vannn1604 <43088114+vannn1604@users.noreply.github.com>
2019-07-21 19:10:19 +0700

    up

---
+++ ocr_utils.py
@@ -0,0 +1,14 @@
+try:
+    from PIL import Image as Image_ocr
+except ImportError:
+    import Image as Image_ocr
+import pytesseract
+
+
+pytesseract.pytesseract.tesseract_cmd = (
+    r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+)
+
+
+def ocr(filename):
+    return pytesseract.image_to_string(Image_ocr.open(filename))
