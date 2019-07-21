5db17abe78b777f15969360ccebff892d2d93100
Your Namevannn1604 <van.nn1604.com>
2019-07-21 11:57:55 +0700

    update

--- ocr_mini/project/views.py
+++ ocr_mini/project/views.py
@@ -1,27 +1,38 @@
+import datetime
+from .models import Project, Image
+from .forms import ProjectForm
+
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Project, Image
 from django.contrib import messages
-import datetime
-from PIL import Image as Image2
-import pytesseract
-# Create your views here.
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-def ocr(filename):
-    return pytesseract.image_to_string(Image2.open(filename))
+from utils.ocr_utils import ocr
 
 
+# import default libs
+# import time, os
+
+# import third party libs
+# import django
+
+# import self module
+# from .models import *
+
+
+
+
+# def upload_image()
+# class ImageModel
 def updateImage(request, project_id, image_id):
     if request.method == "POST":
         img = Image.objects.get(pk=image_id)
-        img.title = request.POST['title']
+        img.title = request.POST["title"]
         img.auto_OCR = False
         img.save()
         project = Project.objects.get(pk=project_id)
         project.updated_at = datetime.datetime.now()
         project.save()
-    return redirect('detailimage', project_id=project_id, image_id=image_id)
+    return redirect("detailimage", project_id=project_id, image_id=image_id)
 
 
 def deleteImage(request, project_id, image_id):
@@ -29,117 +40,163 @@ def deleteImage(request, project_id, image_id):
     project = Project.objects.get(pk=project_id)
     project.updated_at = datetime.datetime.now()
     project.save()
-    return redirect('image', project_id)
+    return redirect("image", project_id)
 
 
 def editImage(request, project_id, image_id):
     img = Image.objects.get(pk=image_id)
     image = {
-        'title': img.title,
-        'created_at': img.created_at,
-        'updated_at': img.updated_at,
-        'auto_OCR': img.auto_OCR,
-        'source': img.source,
-        'project': Project.objects.get(pk=project_id),
-        'image_id': image_id,
-        'username': request.user.username,
+        "title": img.title,
+        "created_at": img.created_at,
+        "updated_at": img.updated_at,
+        "auto_OCR": img.auto_OCR,
+        "source": img.source,
+        "project": Project.objects.get(pk=project_id),
+        "image_id": image_id,
+        "username": request.user.username,
     }
-    return render(request, 'project/editimage.html', image)
+    return render(request, "project/editimage.html", image)
 
 
 def detailImage(request, project_id, image_id):
     img = Image.objects.get(pk=image_id)
     image = {
-        'title': img.title,
-        'created_at': img.created_at,
-        'updated_at': img.updated_at,
-        'auto_OCR': img.auto_OCR,
-        'source': img.source,
-        'project': Project.objects.get(pk=project_id),
-        'image_id': image_id,
-        'username': request.user.username,
+        "title": img.title,
+        "created_at": img.created_at,
+        "updated_at": img.updated_at,
+        "auto_OCR": img.auto_OCR,
+        "source": img.source,
+        "project": Project.objects.get(pk=project_id),
+        "image_id": image_id,
+        "username": request.user.username,
     }
-    return render(request, 'project/detailimage.html', image)
+    return render(request, "project/detailimage.html", image)
 
 
-def uploadingimage(request, project_id):
-    if request.method == 'POST' and request.FILES['source']:
-        source = request.FILES['source']
-        title = ocr(source)
-        image = Image(source=source, title=title, project_id=project_id)
-        image.save()
+def uploadingImage(request, project_id):
+    if request.method == "POST" and "source" in request.FILES:
+        for item in request.FILES.getlist('source'):
+            title = ocr(item)
+            image = Image(source=item, title=title, project_id=project_id)
+            image.save()
         project = Project.objects.get(pk=project_id)
         project.updated_at = datetime.datetime.now()
         project.save()
-    return redirect('image', project_id=project_id)
-        
+    else:
+        messages.warning(request, "You have not selected a file")
+    return redirect("image", project_id=project_id)
+# def uploadingImage(request, project_id):
+#     if request.method == "POST" and "source" in request.FILES:
+#         source = request.FILES["source"]
+#         title = ocr(source)
+#         image = Image(source=source, title=title, project_id=project_id)
+#         image.save()
+#         project = Project.objects.get(pk=project_id)
+#         project.updated_at = datetime.datetime.now()
+#         project.save()
+#     else:
+#         messages.warning(request, "You have not selected a file")
+#     return redirect("image", project_id=project_id)
 
-def creatingproject(request):
-    if request.method == 'POST':
-        title = request.POST['title']
-        description = request.POST['description']
-        request.user.project_set.create(title=title, description=description, user_id=request.user.id)  
 
-    project_id = Project.objects.filter(user_id=request.user.id).last().id
-    return redirect('detailproject', project_id=project_id)
+# def creatingproject(request):
+#     if request.method == 'POST':
+#         title = request.POST['title']
+#         description = request.POST['description']
+#         request.user.project_set.create(title=title, description=description, user_id=request.user.id)
+
+#     project_id = Project.objects.filter(user_id=request.user.id).last().id
+#     return redirect('detailproject', project_id=project_id)
 
 
-def createproject(request):
-    return render(request, 'project/createproject.html', {'username': request.user.username})
+def createProject(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            if Project.objects.filter(title=request.POST["title"]):
+                messages.warning(request, "Project Name Exists")
+                return redirect("createproject")
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            project_id = (
+                Project.objects.filter(user_id=request.user.id).last().id
+            )
+            return redirect("detailproject", project_id=project_id)
+    form = ProjectForm(request.POST)
+    return render(
+        request,
+        "project/createproject.html",
+        {"username": request.user.username, "form": form},
+    )
+
+
+# def createproject(request):
+#     return render(request, 'project/createproject.html', {'username': request.user.username})
 
 
 def project(request):
     # current_user = request.user
-    if request.session.has_key('username'):    
-        list_project = Project.objects.filter(user_id=request.user.id).order_by('-created_at')
-        return render(request, 'project/project.html', {'list_project': list_project, 'username': request.user.username})
-    return redirect('login')
+    if request.session.has_key("username"):
+        list_project = Project.objects.filter(user_id=request.user.id).order_by(
+            "-created_at"
+        )
+        return render(
+            request,
+            "project/project.html",
+            {"list_project": list_project, "username": request.user.username},
+        )
+    return redirect("login")
 
 
 def detailProject(request, project_id):
-    if request.session.has_key('username'):    
+    if request.session.has_key("username"):
         p = Project.objects.get(pk=project_id)
         project = {
-            'title': p.title,
-            'created_at': p.created_at,
-            'updated_at': p.updated_at,
-            'description': p.description,
+            "title": p.title,
+            "created_at": p.created_at,
+            "updated_at": p.updated_at,
+            "description": p.description,
             # 'list_img': p.image_set.all(),
-            'project_id': project_id,
-            'username': request.user.username,
+            "project_id": project_id,
+            "username": request.user.username,
         }
-        return render(request, 'project/detailproject.html', project)
-    return redirect('login')
+        return render(request, "project/detailproject.html", project)
+    return redirect("login")
 
 
 def editProject(request, project_id):
     project = Project.objects.get(pk=project_id)
-    return render(request, 'project/editproject.html', {'project': project, 'username': request.user.username})
+    return render(
+        request,
+        "project/editproject.html",
+        {"project": project, "username": request.user.username},
+    )
 
 
 def updatingProject(request, project_id):
-    if request.method == 'POST':
-        title = request.POST['title']
-        description = request.POST['description']
+    if request.method == "POST":
+        title = request.POST["title"]
+        description = request.POST["description"]
         project = Project.objects.get(pk=project_id)
         project.title = title
         project.description = description
         project.save()
-    return redirect('detailproject', project_id)
+    return redirect("detailproject", project_id)
 
 
 def deleteProject(request, project_id):
     Project.objects.get(pk=project_id).delete()
-    return redirect('project')
+    return redirect("project")
 
 
 def image(request, project_id):
-    if request.session.has_key('username'):
+    if request.session.has_key("username"):
         image = Project.objects.get(pk=project_id).image_set.all()
         content = {
-            'image': image,
-            'username': request.user.username,
-            'project': Project.objects.get(pk=project_id),
+            "image": image,
+            "username": request.user.username,
+            "project": Project.objects.get(pk=project_id),
         }
-        return render(request, 'project/image.html', content)
-    return redirect('login')
+        return render(request, "project/image.html", content)
+    return redirect("login")
--- ocr_mini/user/views.py
+++ ocr_mini/user/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from project.models import Project
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
+
 from .forms import ChangeUser
+from project.models import Project
 # Create your views here.
 
 
@@ -18,6 +19,7 @@ def changeUser(request):
         form = ChangeUser(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Saved Profile Successfully!')
             return redirect('home')
     form = ChangeUser()
     return render(request, 'user/changeuser.html', {'form': form})
@@ -28,8 +30,6 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(
-                request, f'Your account has been created! You are now able to log in')
             return redirect('login')
     form = UserCreationForm()
     return render(request, 'user/register.html', {'form': form})
